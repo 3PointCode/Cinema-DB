2d724331924edc1f72a48c9238cb6006c43e1064
Tomasz Piwoda <tomasz.piwoda@gmail.com>
2025-01-08 13:01:02 +0100

    Changed urls.

--- kino/kino/urls.py
+++ kino/kino/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("repertuar", views.repertuar, name="repertuar"),
+    path("", views.repertuar, name="repertuar"),
     path("filmy", views.filmy, name="filmy"),
     path("seans/<int:id>/", views.seans, name="seans"),
     path("rezerwacjaSukces", views.rezerwacjaSukces, name="rezerwacjaSukces"),
